1392d02f4cd67a73f62fec5fd760f6db53fbb6a9
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2022-08-29 05:28:47 -0400

    added console log

--- netlify/functions/helloWorld.js
+++ netlify/functions/helloWorld.js
@@ -1,4 +1,5 @@
 exports.handler = async function(event, context) {
+    console.log("Invoked with: ", event);
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Hello World!!!"})
